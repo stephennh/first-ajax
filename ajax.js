56d1765827ef0864bb8f1d819d42103c9c190e95
Stephen H <stephen210huang@hotmail.com>
2017-02-16 15:57:36 -0500

    Request to get time zone complete

--- ajax.js
+++ ajax.js
@@ -9,15 +9,41 @@ $(document).ready(function() {
   });
   $('#request-ping').on('click', function() {
     $.ajax({
-      url: 'http://first-ajax-api.herokuapp.com/pong',
+      url: 'http://first-ajax-api.herokuapp.com/ping',
       method: 'GET',
       // data: ,
       dataType: 'text'
     }).done(function(responseData) {
       console.log(responseData);
+      console.log('The request is done.');
       $('#step3456').append(responseData);
     }).fail(function() {
+      console.log('The request failed.');
       $('#step3456').append('Try harder next time!')
+    }).always(function() {
+      console.log('This will always pass.');
+      $('body').append('Hey the request finished!')
+    });
+  });
+  $('#hive-mind').on('click', function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/count',
+      method: 'GET',
+      // data: ,
+      dataType: 'text'
+    }).done(function(responseData) {
+      console.log(responseData);
+      $('#step7').append(responseData);
+    });
+  });
+  $('#send-data').on('click', function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/time',
+      method: 'GET',
+      // data: ,
+      dataType: 'text'
+    }).done(function(responseData) {
+      $('#step8').append(responseData)
     });
   });
 });
