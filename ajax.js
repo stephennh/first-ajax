053fb81e38da9925bf825b886c9b6af7b12c6ed6
Stephen H <stephen210huang@hotmail.com>
2017-02-16 15:37:39 -0500

    Reponse, and when things go wrong - done

--- ajax.js
+++ ajax.js
@@ -1,10 +1,23 @@
-$(document).ready(function () {
-  $('#run-request').on('click',function() {
-    $.ajax ({
+$(document).ready(function() {
+  $('#request-root').on('click', function() {
+    $.ajax({
       url: 'http://first-ajax-api.herokuapp.com/',
       method: 'GET',
       // data: ,
       dataType: 'text'
     });
-  })
+  });
+  $('#request-ping').on('click', function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/pong',
+      method: 'GET',
+      // data: ,
+      dataType: 'text'
+    }).done(function(responseData) {
+      console.log(responseData);
+      $('#step3456').append(responseData);
+    }).fail(function() {
+      $('#step3456').append('Try harder next time!')
+    });
+  });
 });
